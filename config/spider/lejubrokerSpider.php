0f84a2bb3ca5e728623e4a719319d9e35355b962
cnluckylee <cnluckylee@gmail.com>
2014-12-23 01:03:57 +0800

    c

--- config/spider/lejubrokerSpider.php
+++ config/spider/lejubrokerSpider.php
@@ -41,33 +41,16 @@ $siteconfig = array(
                         'ServiceArea'=>'.//div[@class="hall_people_house_font"][1]/text()||2',//区域
                         'Stores' => './/div[@class="hall_people_house_font"][3]/text()||2',//服务楼盘
                         'Tel'=>'.//span[@class="hall_people_red"]/text()||2',
-
                     ),
                 )
-
 		),
 		// item config
-		elements::ITEM_TITLE => '//div[@class="shop"]/span/text()||1',//名称
-		elements::ITEM_SOURCE_CATEGORY_ID => '//div[@class="tel"]/span[@class="num"]/text()||1',//手机
-		elements::ITEM_SOURCE_CATEGORY_NAME=> '//div[@class="aa"]/p[@class="p2"]/text()||1',//所属公司
 		elements::ITEM_SKUID => '//input[@id="talkPageValue"]/@value||1',//昵称
-		elements::ITEM_NAME =>'',
-		elements::ITEM_SOURCE_BRAND_ID =>'',//所属区县
-		elements::ITEM_SOURCE_BRAND_NAME =>'',//服务区域
-		elements::ITEM_SOURCE_SELLER_ID =>'',//昵称
-		elements::ITEM_SOURCE_SELLER_NAME => '',//姓名
-		elements::ITEM_IMAGE_URL => '',
-		elements::ITEM_PROMOTION => '',//注册时间
-		elements::ITEM_SALES => '',//销量
-		elements::ITEM_DPRICE => '',//所属门店
-		elements::ITEM_OPRICE => '',//门店地址
-		elements::ITEM_PRICE_URL => '',//服务热线
-		elements::ITEM_STATUS => '',
-		elements::ITEM_DESCRIPTION =>'',//门店店长
-		elements::ITEM_CHARACTERS =>'',
-		elements::ITEM_ISBN => '//div[@class="main_xiangqing_left"]/p[1]/text()||1',//开店时间
-		elements::ITEM_BARCODE => '',//ID
-		elements::BASE_URL => '//div[@class="favorite"]/a[@class="shouye"]/@href||1',
+        elements::ITEMCOMMON => array(
+            'Tel' => '//div[@class="tel"]/span[@class="num"]/text()||1',
+            'Company' =>'//div[@class="aa"]/p[@class="p2"]/text()||1',//所属公司
+            'UserName' =>'//div[@class="shop"]/span/text()||1',
+        ),
 		elements::STID => 111,
         elements::HTML_ZIP =>'gzip',
 		elements::DATASOURCE => '1',
