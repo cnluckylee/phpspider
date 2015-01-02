f65805f7159ae0d124a23a33901d1977ecefbddd
cnluckylee <cnluckylee@gmail.com>
2015-01-02 21:04:08 +0800

    c
    cc

---
+++ model/soufunesflist2Model.php
@@ -0,0 +1,49 @@
+<?php
+
+class soufunesflist2Model extends spiderModel
+{
+
+    public function  getCategory()
+    {
+        $collection = 'soufunbroker_category_list';
+        $collection_category_name = Application::$_spider [elements::COLLECTION_CATEGORY_NAME];
+        $poolname = $this->spidername . 'Category';
+        $sid = Application::$_spider ['stid'];
+        $result = array();
+        $total = $this->mongodb->count($collection);
+        $s = 0;
+        $limit = 1000;
+        if($total>0)
+            $this->mongodb->remove ( $collection_category_name, array () ); // 删除原始数据，保存最新的数据
+        do {
+            $mondata2 = $result = array ();
+            $mondata = $this->mongodb->find ( $collection, array (), array (
+                "start" => $s,
+                "limit" => $limit
+            ) );
+            foreach($mondata as $item)
+            {
+                $sourceurl = $item['Category_Item_Url'];
+                $arr = parse_url($sourceurl);
+                $baseurl = $arr['scheme']."://".$arr['host'];
+                $result[] = $baseurl.'/agent/agentnew/AloneesfHList.aspx?&agentid='.$item['Category_Item_Name'].'&pricemax=&page=';
+            }
+            $Categorylist = array_unique ( $result );
+
+            foreach ( $Categorylist as $name => $cid ) {
+                $this->pools->set ( $poolname, $cid );
+                $mondata2 [] = array (
+                    'name' => $name,
+                    'cid' => $cid,
+                    'sid' => $sid
+                );
+            }
+            $s +=$limit;
+            if($mondata)
+                $this->mongodb->batchinsert ( $collection_category_name, $mondata2 );
+            unset($mondata);
+
+        }while($s<$total);
+        exit;
+    }
+}
---
+++ model/soufunesflist2ProductModel.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dream 
+ * Date: 14-5-4
+ * Time: 下午5:39
+ */
+
+//namespace model;
+
+
+/**
+ * product model for jin dong website.
+ *
+ * @package model
+ */
+class soufunesflistProductModel extends productXModel {
+    public function getCategoryItemArea()
+    {
+        $str = parent::getCategoryItemArea();
+        $this->_category_item_area = str_replace("/links.htm","",$str);
+        return $this->_category_item_area;
+    }
+
+    public function getCategoryItemOprice()
+    {
+        $arrs = parent::getCategoryItemOprice();
+        $this->_category_item_oprice = array();
+        foreach($arrs as $k=>$v)
+        {
+            $this->_category_item_oprice[$k] = str_replace(array(" ","\r\n"),"",$v);
+        }
+        return $this->_category_item_oprice;
+    }
+
+    public function getCategoryItemSale()
+    {
+        $arrs = parent::getCategoryItemSale();
+        $this->_category_item_sale = array();
+        foreach($arrs as $k=>$v)
+        {
+            $this->_category_item_sale[$k] = str_replace(array(" ","\r\n"),"",$v);
+        }
+        return $this->_category_item_sale;
+    }
+
+    public function getCategoryItemDprice()
+    {
+        $filter = $this->_config[\elements::CATEGORY_ITEM_DPRICE];
+        $nodes = $this->_xpath->query($filter);
+        $this->_category_item_dprice = array();
+        foreach ($nodes as $k=>$node) {
+            foreach ($this->_xpath->query('.//dl/dt/p[2]/text()', $node) as $child) {
+                $this->_category_item_dprice[$k] = trim(str_replace(array(" ","\r\n"),array(""," "),$child->nodeValue));
+            }
+        }
+        return $this->_category_item_dprice;
+    }
+
+    public function getCategoryItemHot()
+    {
+        $arrs = parent::getCategoryItemHot();
+        $this->_category_item_hot = array();
+        foreach($arrs as $k=>$v)
+        {
+            $this->_category_item_hot[$k] = str_replace("更新时间：","",$v);
+        }
+        return $this->_category_item_hot;
+    }
+
+    public  function getCategoryItemUrL()
+    {
+        $sourceurl = parent::getUrl();
+        $data = parent::getCategoryItemUrL();
+        $arr = parse_url($sourceurl);
+        $baseurl = $arr['scheme']."://".$arr['host'];
+        $this->_category_item_url = array();
+        foreach($data as $k=>$v)
+        {
+            $url = trim($v);
+            $skuid = str_replace("/a/","",$v);
+            $this->_category_item_url[$k] = $baseurl.'/agent/Agentnew/AloneService.aspx?managername='.$skuid;
+        }
+        return  $this->_category_item_url;
+    }
+
+    public function  getAllCommentNumber()
+    {
+
+        $filter = $this->_config[\elements::ITEM_COMMENT_NUMBER_ALL];
+        $nodes = $this->_xpath->query($filter);
+
+        foreach($nodes as $node)
+        {
+            $str = $node->textContent;
+            $arr = explode("发布时间：",$str);
+            $arr = explode("(",$arr[1]);
+            $this->_allCommentNumber = date('Y-m-d H:i:s',strtotime($arr[0]));
+        }
+        return $this->_allCommentNumber;
+    }
+
+    public function  getDissatisfyCommentNumber()
+    {
+        $filter = $this->_config[\elements::ITEM_COMMENT_NUMBER_ALL];
+        $nodes = $this->_xpath->query($filter);
+        foreach($nodes as $node)
+        {
+            $str = $node->textContent;
+            $arr = explode("发布时间：",$str);
+            $arr = explode("(",$arr[1]);
+
+            $num = intval($arr[1]);
+            $c = '';
+            if(strpos($arr[1],'天'))
+            {
+                $c = 'day';
+            }else  if(strpos($arr[1],'时'))
+            {
+                $c = 'hour';
+            }else  if(strpos($arr[1],'月'))
+            {
+                $c = 'month';
+            }
+            $this->_dissatisfyCommentNumber = date('Y-m-d H:i:s',strtotime("-".$num.$c));
+        }
+
+        return $this->_dissatisfyCommentNumber;
+    }
+
+    public function  getGeneralCommentNumber()
+    {
+        $filter = $this->_config[\elements::ITEM_COMMENT_NUMBER_ALL];
+        $nodes = $this->_xpath->query($filter);
+        foreach($nodes as $node)
+        {
+            $str = $node->textContent;
+            $arr = explode("发布时间：",$str);
+            $this->_generalCommentNumber = $arr[1];
+        }
+        return $this->_generalCommentNumber;
+    }
+
+    public function getTitle()
+    {
+        $str = parent::getTitle();
+        $this->_title = trim(str_replace(array("\r\n"),"",$str));
+        return $this->_title;
+    }
+
+    public function getMPrice()
+    {
+
+        if (is_null($this->_mprice)) {
+            $filter = $this->_config[\elements::ITEM_MPRICE];
+            $this->_mprice = $this->_getRegexpInfo($filter, $this->getContent());
+        }
+        return $this->_mprice;
+
+    }
+
+}
