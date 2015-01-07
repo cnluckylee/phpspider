e1fc91f4fe9f8c261fa021c7cc81981599f32748
cnluckylee <cnluckylee@gmail.com>
2015-01-07 19:29:03 +0800

    c

--- model/soufunesflistModel.php
+++ model/soufunesflistModel.php
@@ -140,6 +140,7 @@ class soufunesflistModel extends spiderModel
                                 $item[\elements::CATEGORY_ITEM_URL] = $job.$item['houseurl'];
                                 $item[\elements::CATEGORY_ITEM_SKUID] = $item['houseid'];
                                 $item['registtime'] = $this->tools->getSourceUpdateTime($item['registdate']);
+                                $item['create_time'] = date('Y-m-d H:i:s');
                                 $item['job'] = $rurl;
                                 if($item[\elements::CATEGORY_ITEM_URL])
                                     $this->pools->set ( $poolname, $item[\elements::CATEGORY_ITEM_URL] );//将category_item_url加入任务池中 2014.12.20 22:32
